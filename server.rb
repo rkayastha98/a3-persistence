ce3d1b435a0f293f33e4573e355680f1c5e99ec8
rkayastha98 <rkayastha@wpi.edu>
2020-10-17 20:42:28 -0400

    initial

--- server.rb
+++ server.rb
@@ -6,5 +6,5 @@ CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
 CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
 
 get '/' do
-  erb :index, :locals => {:client_id => CLIENT_ID}
+  html :index, :locals => {:client_id => CLIENT_ID}
 end
