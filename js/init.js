d645ee39a6ccd4904959465875519a7106644205
rkayastha98 <rkayastha@wpi.edu>
2020-10-20 19:35:34 -0400

    index.html

--- js/init.js
+++ js/init.js
@@ -1,8 +1,34 @@
+
+
 (function($){
   $(function(){
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
+    /**
+     * 1. server setup locally
+     * 2. check if user is logged in
+     * 3. if not logged in redirect to login.html
+     * 4. if logged in its fine in index.html
+     */
+
+
+    /**
+     * 1. redirect to github login page
+     * 2. fetch the access token from github
+     * 3. store the access token in browser's localstorage
+     * 4. use access token check in index.html
+     *
+     */
+
+
+
+    // $('.sidenav').sidenav();
+    // $('.parallax').parallax();
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function exit() {
+  localStorage.clear();
+  window.location.href = '/login.html';
+}
--- js/login.js
+++ js/login.js
@@ -1,6 +1,60 @@
-"use strict";
+// "use strict";
+//
+// document.getElementById("github-login-button").addEventListener("click", evt => {
+//     evt.preventDefault();
+//     window.location.href = "/auth/github/login";
+// });
+//
+// const express = require('express')
+// const app = express()
+//
+// const axios = require('axios')
+//
+// const clientID = '<GITHUB_CLIENT_ID>'
+// const clientSecret = '<GITHUB_CLIENT_SECRET>'
+//
+//
+// app.get('/home', (req, res) => {
+//
+//     // The req.query object has the query params that were sent to this route.
+//     const requestToken = req.query.code
+//
+//     axios({
+//         method: 'post',
+//         url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+//         // Set the content type header, so that we get the response in JSON
+//         headers: {
+//             accept: 'application/json'
+//         }
+//
+//     }).then((response) => {
+//
+//         const accessToken = response.data.access_token
+//         console.log(response.data)
+//
+//         // redirect the user to the home page, along with the access token
+//         res.redirect(`/home.html?access_token=${accessToken}`)
+//     })
+// })
 
-document.getElementById("github-login-button").addEventListener("click", evt => {
-    evt.preventDefault();
-    window.location.href = "/auth/github/login";
-});
+// app.use(express.static(__dirname + '/public'))
+// app.listen(4000,()=>{
+//     console.log("Server listening on port : 4000")
+// })
+
+$(document).ready(function(){
+
+    //when redirected from github login page
+    let searchParams = new URLSearchParams(window.location.search)
+    let access_token = searchParams.get('code');
+    if(access_token){
+        localStorage.setItem('access_token', access_token);
+        window.location.href = '/index.html';
+    }
+
+    //checking if access token exists
+    let local_access_token = localStorage.getItem('access_token');
+    if(local_access_token){
+        window.location.href = '/index.html';
+    }
+})
